83443dfaad895270b239fc59bc388cc3297ace29
Thefriendlymoose <alamiridavid@gmail.com>
2022-09-07 09:34:52 +0200

    test connection to mongoDB cloud

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,8 +1,16 @@
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.net.UnknownHostException;
 
 public class HelloWorld extends Application {
 
@@ -11,11 +19,38 @@ public class HelloWorld extends Application {
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+    public void start(Stage stage) {
+
+        MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb+srv://WMSProject:grupp19@wmsproject.tlzpnr0.mongodb.net/?retryWrites=true&w=majority"));
+
+
+        stage.setTitle("WMS");
+        MenuBar menuBar = new MenuBar();
+        VBox vBox = new VBox(menuBar);
+
+        Menu file = new Menu("File");
+        MenuItem add = new MenuItem("Edit");
+        MenuItem delete = new MenuItem("Delete");
+        SeparatorMenuItem separator = new SeparatorMenuItem();
+        MenuItem logIn = new MenuItem("Log In");
+        MenuItem logOut = new MenuItem("Log Out");
+
+        file.getItems().add(add);
+        file.getItems().add(delete);
+        file.getItems().add(separator);
+        file.getItems().add(logIn);
+        file.getItems().add(logOut);
+
+        Menu edit = new Menu("Edit");
+        Menu view = new Menu("View");
+        Menu help = new Menu("Help");
+
+        menuBar.getMenus().add(file);
+        menuBar.getMenus().add(edit);
+        menuBar.getMenus().add(view);
+        menuBar.getMenus().add(help);
+
+        Scene scene = new Scene(vBox, 640, 480);
         stage.setScene(scene);
         stage.show();
     }
