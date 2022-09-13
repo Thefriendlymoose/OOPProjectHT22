8e1d4b2fcdf35c9bf1f08c464bfca6ca64865b7f
Thefriendlymoose <alamiridavid@gmail.com>
2022-09-13 11:23:48 +0200

    changed the mongodb java driver to newer version, tested adding a document to the collection

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,13 +1,22 @@
 
-import com.mongodb.ConnectionString;
+
+import com.mongodb.*;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.InsertOneResult;
+
 
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import java.util.Arrays;
 
 
 public class HelloWorld extends Application {
@@ -19,15 +28,6 @@ public class HelloWorld extends Application {
     @Override
     public void start(Stage stage) {
         String MONGO_URI = "mongodb+srv://WMSProject:grupp19@wmsproject.tlzpnr0.mongodb.net/?retryWrites=true&w=majority";
-        MongoClient mongoClient = MongoClients.create(MONGO_URI);
-
-        try{
-            System.out.println("MongoDB Server is up:- "+mongoClient.getClusterDescription());
-        } catch (Exception e){
-            System.out.println("Failed to connect");
-        }
-
-        System.out.println(mongoClient.getDatabase("WMS").getName());
 
 
         stage.setTitle("WMS");
@@ -40,12 +40,14 @@ public class HelloWorld extends Application {
         SeparatorMenuItem separator = new SeparatorMenuItem();
         MenuItem logIn = new MenuItem("Log In");
         MenuItem logOut = new MenuItem("Log Out");
+        MenuItem info = new MenuItem("Info");
 
         file.getItems().add(add);
         file.getItems().add(delete);
         file.getItems().add(separator);
         file.getItems().add(logIn);
         file.getItems().add(logOut);
+        file.getItems().add(info);
 
         Menu edit = new Menu("Edit");
         Menu view = new Menu("View");
@@ -59,5 +61,30 @@ public class HelloWorld extends Application {
         Scene scene = new Scene(vBox, 640, 480);
         stage.setScene(scene);
         stage.show();
+
+        ConnectionString connectionString = new ConnectionString("mongodb+srv://WMS-001:grupp19@wmsproject.tlzpnr0.mongodb.net/?retryWrites=true&w=majority");
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .serverApi(ServerApi.builder()
+                        .version(ServerApiVersion.V1)
+                        .build())
+                .build();
+
+
+
+        try (MongoClient mongoClient = MongoClients.create(settings);) {
+            MongoDatabase database = mongoClient.getDatabase("WMS");
+            MongoCollection<Document> collection = database.getCollection("test");
+            System.out.println("Connected");
+            try {
+                InsertOneResult result = collection.insertOne(new Document()
+                        .append("_id", new ObjectId())
+                        .append("name", "james")
+                        .append("age","26"));
+                System.out.println("Success! Inserted document id: " + result.getInsertedId());
+            } catch (MongoException me) {
+                System.err.println("Unable to insert due to an error: " + me);
+            }
+        }
     }
 }
