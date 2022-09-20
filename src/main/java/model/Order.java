571c077b4ea6c2eec726bdb3355902a59846c422
jplsson <james.palsson@gmail.com>
2022-09-20 13:52:45 +0200

    pushing order things

---
+++ src/main/java/model/Order.java
@@ -0,0 +1,44 @@
+package model;
+
+import model.pojos.OrderPojo;
+import org.bson.codecs.pojo.annotations.BsonProperty;
+import org.bson.types.ObjectId;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Order {
+
+//    @BsonProperty("_objectId")
+    private String objectId;
+//    private final org.bson.types.ObjectId ObjectId;
+    private OrderPojo orderPojo;
+
+    private ObjectId id;
+
+    private List<Article> articles; //inventory ???
+
+    private String articleDescription;
+
+    private LocalDateTime createdDate;
+
+    private String status;
+
+    private String user;
+
+    private String site;
+
+    private LocalDateTime lastEdited;
+    
+    private Order() {
+        
+    }
+
+    
+
+    enum OrderStatus {
+        ACTIVE,
+        CANCELED,
+        FINISHED
+    }
+}
