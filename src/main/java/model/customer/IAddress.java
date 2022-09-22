9121b8d25980747516d9bcdba5b1ba4fbfd9cd8d
doktorjevsky <97901243+doktorjevsky@users.noreply.github.com>
2022-09-22 22:06:01 +0200

    adding setters and creating concrete classes (2)

--- src/main/java/model/customer/IAddress.java
+++ src/main/java/model/customer/IAddress.java
@@ -2,9 +2,18 @@ package model.customer;
 
 public interface IAddress {
 
+    //getters
     String getStreetName();
     int getStreetNumber();
     String getPostalCode();
     String getCityName();
     String getCountry();
+
+    //setters
+
+    void setStreetName(String streetName);
+    void setStreetNumber(int streetNumber);
+    void setPostalCode(String postalCode);
+    void setCityName(String cityName);
+    void setCountry(String country);
 }
