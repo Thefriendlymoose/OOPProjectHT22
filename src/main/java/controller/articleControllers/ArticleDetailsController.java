81a4b8eb38a177bb21312387b6c969951a906753
Thefriendlymoose <alamiridavid@gmail.com>
2022-09-24 00:16:09 +0200

    finished the FXMLs for article details and article form, also created CSS for the ComboBox, added to the controllers for the form and details windows, still need to add connection to the model so that the data can be populated

--- src/main/java/controller/articleControllers/ArticleDetailsController.java
+++ src/main/java/controller/articleControllers/ArticleDetailsController.java
@@ -1,5 +1,46 @@
 package controller.articleControllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import model.article.ArticleCategory;
+import model.article.ArticleStatus;
+
+
 public class ArticleDetailsController {
+    @FXML
+    Label detailsTitleLabel, numberLabel, nameLabel, descriptionLabel, categoryLabel, statusLabel;
+
+    @FXML
+    TextField numberTextField, nameTextField;
+
+    @FXML
+    TextArea descriptionTextArea;
+
+    @FXML
+    ComboBox<ArticleCategory> categoryComboBox;
+
+    @FXML
+    ComboBox<ArticleStatus> statusComboBox;
+
+    @FXML
+    Button editButton, closeButton;
+
+    @FXML
+    public void initialize(){
+
+        // TODO Ask model for data user is asking for and present by populating the fields
+
+        detailsTitleLabel.setText(detailsTitleLabel.getText() + "test");
+        numberTextField.setText("123456789");
+        nameTextField.setText("Test Article");
+        descriptionTextArea.setText("This is a test description of the test article, it is 0x0x0");
+        categoryComboBox.setValue(ArticleCategory.Office);
+        statusComboBox.setValue(ArticleStatus.Active);
+        categoryComboBox.getItems().setAll(ArticleCategory.values());
+        statusComboBox.getItems().setAll(ArticleStatus.values());
+
+    }
+
+    //TODO handlers for when user clicks edit and close buttons
 
 }
--- src/main/java/controller/articleControllers/ArticleFormController.java
+++ src/main/java/controller/articleControllers/ArticleFormController.java
@@ -1,4 +1,49 @@
 package controller.articleControllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import model.article.ArticleCategory;
+import model.article.ArticleStatus;
+
 public class ArticleFormController {
+
+    @FXML
+    Label titleLabel;
+
+    @FXML
+    TextField numberTextField, nameTextField;
+
+    @FXML
+    TextArea descriptionTextArea;
+
+    @FXML
+    ComboBox<ArticleCategory> categoryComboBox;
+
+    @FXML
+    ComboBox<ArticleStatus> statusComboBox;
+
+    @FXML
+    Button saveButton, cancelButton;
+
+
+    @FXML
+    public void initialize(){
+        titleLabel.setText("Create Article");
+
+        // TODO: Need the model to autogenerate the article number/ID
+        numberTextField.setText("123451234");
+
+        categoryComboBox.getItems().setAll(ArticleCategory.values());
+        statusComboBox.getItems().setAll(ArticleStatus.values());
+
+    }
+
+    public void onSave(){
+        // TODO: Need to check all fields, then create new object from data?
+
+    }
+
+    public void onCancel(){
+        // TODO prompt user ask if sure
+    }
 }
