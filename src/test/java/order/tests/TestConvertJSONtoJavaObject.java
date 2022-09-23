158365e2a6f1d7220c7816ca392cf12af017c5c7
jplsson <james.palsson@gmail.com>
2022-09-23 11:55:31 +0200

    orders for test

---
+++ src/test/java/order/tests/TestConvertJSONtoJavaObject.java
@@ -0,0 +1,16 @@
+package order.tests;
+
+import org.junit.jupiter.api.Test;
+
+//import java.util.Map;
+
+public class TestConvertJSONtoJavaObject {
+
+    @Test
+    public void testConvert(){
+
+//        ObjectMapper mapper = new ObjectMapper();
+//        Map<String,Object> map = mapper.readValue(json, Map.class);
+
+    }
+}
