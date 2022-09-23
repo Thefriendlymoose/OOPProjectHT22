5eea1360677ba930ba0cc9309062995df7bba0eb
AlexanderHyyppa1337 <alexander.hyyppa@gmail.com>
2022-09-23 13:11:42 +0200

    something

--- src/main/java/controller/userControllers/UserMenuController.java
+++ src/main/java/controller/userControllers/UserMenuController.java
@@ -13,9 +13,36 @@ public class UserMenuController {
     @FXML
     private Button openButton, createButton, listButton, backButton;
 
+    public void createButtonHandler() throws Exception {
+        changeScene("CreateUserMenu", createButton);
+    }
+    /*
+
+    public void orderBtnHandler() throws Exception {
+        changeScene("orderViews/orderMenu", menuOrderButton);
+    }
+
+    public void customerBtnHandler() throws Exception {
+        changeScene("customerViews/customerMenu", menuCustomerButton);
+    }
+
+    public void userBtnHandler() throws Exception {
+        changeScene("userViews/userMenu", menuUserButton);
+    }
+    public void siteBtnHandler() throws Exception {
+        changeScene("siteViews/siteMenu", menuSiteButton);
+    }
+
+     */
+
     public void backBtnHandler() throws Exception{
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../../fxml/mainMenu.fxml")));
         Stage window = (Stage) backButton.getScene().getWindow();
+        window.setScene(new Scene(root));
+    }
+    private void changeScene(String newScene, Button button) throws Exception{
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../../fxml/userViews/"+newScene+".fxml")));
+        Stage window = (Stage) button.getScene().getWindow();
         window.setScene(new Scene(root));
     }
 }
