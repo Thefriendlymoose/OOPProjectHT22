fbf956839b015efc9d0385c9f23244230bfbac5f
doktorjevsky <97901243+doktorjevsky@users.noreply.github.com>
2022-10-05 13:18:00 +0200

    started making customer view (4)

--- src/main/java/controller/customerControllers/CustomerCreateController.java
+++ src/main/java/controller/customerControllers/CustomerCreateController.java
@@ -1,9 +1,16 @@
 package controller.customerControllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class CustomerCreateController {
 
@@ -15,4 +22,12 @@ public class CustomerCreateController {
 
     @FXML
     Button saveButton, cancelButton, editShippingAddress, editBillingAddress, editContacts;
+
+    public void addShippingAddressHandler(ActionEvent e) {
+        Stage stage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../../fxml/customerViews/addressCreate.fxml")));
+        stage.setTitle("Create Address");
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.initOwner(((Node)e.getSource()).getScene().getWindow());
+        stage.show();
+    }
 }
