58ce3eb5afdd8629142773350dfe8b0d94eda686
Thefriendlymoose <alamiridavid@gmail.com>
2022-10-11 21:30:16 +0200

    clean up of some unneeded FXML and controllers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,8 @@ import javafx.stage.Stage;
 import model.Authentication.AuthenticationStatus;
 import model.Authentication.UserAuthentication;
 import model.article.Articles;
+import model.order.Orders;
+import model.site.Sites;
 import persistence.*;
 
 import java.time.LocalDateTime;
@@ -13,18 +15,37 @@ import java.util.HashMap;
 
 
 public class Main extends Application {
+
+    private Articles articles;
+    private Orders orders;
+    private Sites sites;
+
+
+
     public static void main(String[] args){
         launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
+        this.articles = new Articles(ArticlesDAO.getInstance().getAllMap());
+        this.orders = new Orders(OrderDAO.getInstance().getAllMap());
+        this.sites = new Sites(SitesDAO.getInstance().getAllMap());
 
-        Parent root = FXMLLoader.load(getClass().getResource("fxml/template.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml/startScreen.fxml"));
+        Parent root = loader.load();
+
+
         Scene scene = new Scene(root);
         stage.setTitle("WMS");
         stage.setScene(scene);
         stage.show();
 
     }
+
+    @Override
+    public void stop(){
+
+    }
+
 }
