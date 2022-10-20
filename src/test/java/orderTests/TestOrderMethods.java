a30cab9191c3340552a8778ca317299f801a4059
jplsson <james.palsson@gmail.com>
2022-10-20 13:32:39 +0200

    some tests

--- src/test/java/orderTests/TestOrderMethods.java
+++ src/test/java/orderTests/TestOrderMethods.java
@@ -6,9 +6,8 @@ import model.article.ArticleStatus;
 import model.order.OrderRow;
 import model.order.Order;
 import model.order.OrderStatus;
+import model.site.Site;
 import model.site.SiteArticle;
-import org.junit.BeforeClass;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -23,25 +22,32 @@ public class TestOrderMethods {
     Article article3 = new Article(3,"Name 3", "Desc 3", ArticleCategory.Beds, ArticleStatus.Active,3,4,null,null,null);
     OrderRow orderRow1 = new OrderRow(article1,1);
     OrderRow orderRow2 = new OrderRow(article2,2);
+    OrderRow orderRow3 = new OrderRow(article1,3);
+
+    SiteArticle siteArticle1 = new SiteArticle(article1,1);
+    SiteArticle siteArticle2 = new SiteArticle(article2,2);
+
     Order order1 = new Order(null,11,null,OrderStatus.ACTIVE,true,null,null,null,null);
+    List<SiteArticle> siteArticles = new ArrayList<>();
+    Site site1 = new Site(1,"Name 1","Adress 1",5,siteArticles,null);
 
 
-    @BeforeEach
-    public void initEach(){
-        List<OrderRow> dummyOrderRows = new ArrayList<>();
-        Article article1 = new Article(1,"Name 1", "Desc 1", ArticleCategory.Beds, ArticleStatus.Active,1,2,null,null,null);
-        Article article2 = new Article(2,"Name 2", "Desc 2", ArticleCategory.Lighting, ArticleStatus.Discontinued,2,3,null,null,null);
-        Article article3 = new Article(3,"Name 3", "Desc 3", ArticleCategory.Electronics, ArticleStatus.Limited,3,4,null,null,null);
-
-
-        dummyOrderRows.add(new OrderRow(article1,1));
-        dummyOrderRows.add(new OrderRow(article2,2));
-        dummyOrderRows.add(new OrderRow(article3,3));
-
-//        List<Order> dummyOrders = new ArrayList<>();
-//        Order order1 = new Order(null,1,null, OrderStatus.ACTIVE,true,null,null,dummyOrderRows,null);
-        System.out.println("BeforeEach initEach() method called");
-    }
+//    @BeforeEach
+//    public void initEach(){
+//        List<OrderRow> dummyOrderRows = new ArrayList<>();
+//        Article article1 = new Article(1,"Name 1", "Desc 1", ArticleCategory.Beds, ArticleStatus.Active,1,2,null,null,null);
+//        Article article2 = new Article(2,"Name 2", "Desc 2", ArticleCategory.Lighting, ArticleStatus.Discontinued,2,3,null,null,null);
+//        Article article3 = new Article(3,"Name 3", "Desc 3", ArticleCategory.Electronics, ArticleStatus.Limited,3,4,null,null,null);
+//
+//
+//        dummyOrderRows.add(new OrderRow(article1,1));
+//        dummyOrderRows.add(new OrderRow(article2,2));
+//        dummyOrderRows.add(new OrderRow(article3,3));
+//
+////        List<Order> dummyOrders = new ArrayList<>();
+////        Order order1 = new Order(null,1,null, OrderStatus.ACTIVE,true,null,null,dummyOrderRows,null);
+//        System.out.println("BeforeEach initEach() method called");
+//    }
 
 
     @Test void totalRevenue(){
@@ -73,7 +79,7 @@ public class TestOrderMethods {
         List<OrderRow> dummyOrderRows = new ArrayList<>();
         dummyOrderRows.add(orderRow1);
 
-        SiteArticle siteArticle1 = new SiteArticle(article1,1);
+
 
         Order order1 = new Order(null,1,null, OrderStatus.ACTIVE,true,null,null,dummyOrderRows,null);
 
@@ -87,55 +93,75 @@ public class TestOrderMethods {
 
     @Test
     public void reducingOrderRow1(){
-        List<OrderRow> dummyOrderRows = new ArrayList<>();
-        dummyOrderRows.add(orderRow1);
-        dummyOrderRows.add(orderRow2);
 
+//        Test to satisfy
 //        if (or.getAmount() == amount){
 //            orderRows.remove(or);
 //            return site.addSiteArticle(art, amount);
 
-        Order order1 = new Order(null,1,null, OrderStatus.ACTIVE,true,null,null,dummyOrderRows,null);
+        List<OrderRow> dummyOrderRows = new ArrayList<>();
+        dummyOrderRows.add(orderRow1);
+        dummyOrderRows.add(orderRow2);
+
+
+        site1.addSiteArticle(article1,1);
+        Order order1 = new Order(null,1,null, OrderStatus.ACTIVE,true,null,null,dummyOrderRows,site1);
+
+
+        assertTrue(order1.reduceOrderRow(orderRow1,1));
 
     }
 
     @Test
     public void reducingOrderRow2(){
+//        Test to satisfy
+//        if (or.getAmount() < amount || amount < 0){
+//            return false;
+
         List<OrderRow> dummyOrderRows = new ArrayList<>();
         dummyOrderRows.add(orderRow1);
         dummyOrderRows.add(orderRow2);
 
-//        if (or.getAmount() < amount || amount < 0){
-//            return false;
 
-        Order order1 = new Order(null,1,null, OrderStatus.ACTIVE,true,null,null,dummyOrderRows,null);
+        site1.addSiteArticle(article1,1);
+        Order order1 = new Order(null,1,null, OrderStatus.ACTIVE,true,null,null,dummyOrderRows,site1);
+
+
+        assert(!order1.reduceOrderRow(orderRow1,2));
 
     }
 
     @Test
     public void reducingOrderRow3(){
-        List<OrderRow> dummyOrderRows = new ArrayList<>();
-        dummyOrderRows.add(orderRow1);
-        dummyOrderRows.add(orderRow2);
 
-//       if (site.checkIfOverCapacity(amount)) {
+//        Test to satisfy
+//        if (site.checkIfOverCapacity(amount))
 //        return false;
 
-        Order order1 = new Order(null,1,null, OrderStatus.ACTIVE,true,null,null,dummyOrderRows,null);
-    }
-
-    @Test
-    public void reducingOrderRow4(){
         List<OrderRow> dummyOrderRows = new ArrayList<>();
         dummyOrderRows.add(orderRow1);
         dummyOrderRows.add(orderRow2);
 
-//      else {
-//            or.reduceAmount(amount);
-//            return site.addSiteArticle(art, amount);
-//           }
 
-        Order order1 = new Order(null,1,null, OrderStatus.ACTIVE,true,null,null,dummyOrderRows,null);
-    }
+        site1.addSiteArticle(article1,1);
+        Order order1 = new Order(null,1,null, OrderStatus.ACTIVE,true,null,null,dummyOrderRows,site1);
+
+
+        assert(!order1.reduceOrderRow(orderRow3,999999999));
+}
+
+//    @Test
+//    public void reducingOrderRow4(){
+//        List<OrderRow> dummyOrderRows = new ArrayList<>();
+//        dummyOrderRows.add(orderRow1);
+//        dummyOrderRows.add(orderRow2);
+//
+////      else {
+////            or.reduceAmount(amount);
+////            return site.addSiteArticle(art, amount);
+////           }
+//
+//        Order order1 = new Order(null,1,null, OrderStatus.ACTIVE,true,null,null,dummyOrderRows,null);
+//    }
 
 }
